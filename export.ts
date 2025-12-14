f77fc816ab3cfd172090bd8a077752a2b5d892c8
kurenai-11 <moldrenkailich@gmail.com>
2025-12-14 14:12:23 +0300

    fixed some margin after the dic

--- export.ts
+++ export.ts
@@ -4,8 +4,8 @@ import { processGuifan } from "./dics/guifan/guifan.ts";
 import { processHanyu7 } from "./dics/hanyu7/hanyu7.ts";
 
 const versions = {
-  guifan: "2025/12/14.7",
-  hanyu7: "2025/12/14.7",
+  guifan: "2025/12/14.8",
+  hanyu7: "2025/12/14.8",
 };
 
 const guifanPinyinDic = new Dictionary({ fileName: "guifan-pinyin.zip" });
