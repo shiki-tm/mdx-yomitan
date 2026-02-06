4910f12efa6a5808733adafbff5f71606592c3ab
kurenai-11 <moldrenkailich@gmail.com>
2026-02-06 18:33:12 +0300

    [Hanyu7] forgot to apply the same styles as f21f990 to the pinyin css too

--- export.ts
+++ export.ts
@@ -5,7 +5,7 @@ import { processHanyu7 } from "./dics/hanyu7/hanyu7.ts";
 
 const versions = {
   guifan: "2026/02/05.4",
-  hanyu7: "2026/02/05.4",
+  hanyu7: "2026/02/05.5",
 };
 
 const guifanPinyinDic = new Dictionary({ fileName: "guifan-pinyin.zip" });
